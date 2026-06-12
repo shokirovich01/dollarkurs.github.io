47a1496740f42831ed95c41d9b1635a34b062dc8
shokirovich01 <surfacelaptop77@gmail.com>
2026-06-12 10:58:13 -1000

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,89 @@
-<div class="bg"></div>
-<div class="money-container"></div>
+const API = "https://open.er-api.com/v6/latest/USD";
 
-<h1>💱 Dollar Kurs shokirovich</h1>
+let rates = {};
+let selected = "UZS";
 
-<input id="amount" type="number" placeholder="USD kiriting">
+const container = document.querySelector(".money-container");
 
-<h2 id="result"></h2>
+// 🌍 kurs olish
+async function getRates(){
+  const res = await fetch(API);
+  const data = await res.json();
+  rates = data.rates;
 
-<div id="rates"></div>
+  showRates();
+  autoConvert();
+}
+
+// 📊 kurslar
+function showRates(){
+
+  const list = [
+    {name:"🇺🇿 O‘zbekiston", cur:"UZS"},
+    {name:"🇺🇸 AQSh", cur:"USD"},
+    {name:"🇪🇺 Yevropa", cur:"EUR"},
+    {name:"🇷🇺 Rossiya", cur:"RUB"}
+  ];
+
+  const box = document.getElementById("rates");
+  box.innerHTML = "";
+
+  list.forEach(c=>{
+    box.innerHTML += `
+      <div class="card">
+        ${c.name} <br>
+        1 USD = ${rates[c.cur]} ${c.cur}
+      </div>
+    `;
+  });
+}
+
+// 🧮 real-time convert
+document.getElementById("amount").addEventListener("input", autoConvert);
+
+function autoConvert(){
+  let amount = document.getElementById("amount").value;
+
+  if(!amount){
+    document.getElementById("result").innerText = "";
+    return;
+  }
+
+  let result = amount * rates[selected];
+
+  document.getElementById("result").innerText =
+    `${amount} USD = ${result.toLocaleString()} ${selected}`;
+}
+
+// 💵 flying money
+function createMoney(){
+  const el = document.createElement("div");
+  el.classList.add("money");
+
+  el.innerText = "💵";
+
+  el.style.left = Math.random() * window.innerWidth + "px";
+  el.style.animationDuration = (3 + Math.random() * 4) + "s";
+  el.style.fontSize = (20 + Math.random() * 30) + "px";
+
+  container.appendChild(el);
+
+  setTimeout(()=>el.remove(), 8000);
+}
+
+// 🔄 start
+setInterval(getRates, 3000);
+setInterval(createMoney, 300);
+
+getRates();
+const CRYPTO_API = "https://api.coingecko.com/api/v3/simple/price?ids=bitcoin,ethereum,binancecoin,solana,ripple&vs_currencies=usd";
+
+let crypto = {};
+
+async function getCrypto(){
+  const res = await fetch(CRYPTO_API);
+  const data = await res.json();
+  crypto = data;
+
+  showCrypto();
+}
